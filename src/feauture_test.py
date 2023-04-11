d545e87d94c8eaeda505279fbd35efe0e8300900
sorinar329 <sorin@uni-bremen.de>
2023-04-11 17:00:03 +0200

    Added new functions to adjust the correct object ids

--- src/feauture_test.py
+++ src/feauture_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import shutil
 import json
+#import utils
 
 #pydevd_pycharm.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True)
 shutil.rmtree("/home/sorin/code/blenderproc/output2/coco_data")
@@ -13,44 +14,41 @@ def create_list_from_id2json(path='/home/sorin/code/blenderproc/data/id2name.jso
     with open(path) as json_file:
         data = json.load(json_file)
 
-    val = list(data.values())
-    i = 0
-    new_list = []
-    for a in val:
-        if a.startswith("suturo"):
-            item = a.split(":")
-            item_to_list = (item[1]).replace("'", "")
-            new_list.append(item_to_list)
-        i = i + 1
+        val = list(data.values())
+        i = 0
+        new_list = []
+        for a in val:
+            if a.startswith("suturo"):
+                item = a.split(":")
+                item_to_list = (item[1]).replace("'", "")
+                new_list.append(item_to_list)
+            i = i + 1
 
-    return new_list
+        return new_list
 
 
 def get_id_of_object(obj):
     with open('/home/sorin/code/blenderproc/data/id2name.json') as json_file:
         data = json.load(json_file)
 
-    val = list(data.values())
-    i = 0
-    for item in val:
-        if item.endswith(obj + "'"):
-            id_of_obj = i
-            break
-        else:
-            i = i + 1
+        val = list(data.values())
+        i = 0
+        for item in val:
+            if item.endswith(obj + "'"):
+                id_of_obj = i
+                break
+            else:
+                i = i + 1
 
-    return i
+        return i
 
 
 
 # Create a simple object:
-objs = bproc.loader.load_obj("/home/sorin/data/blenderproc/data/saved_scenes/testing.obj")
-
-
+objs = bproc.loader.load_obj("/home/sorin/data/blenderproc/data/saved_scenes/suturobocup_scene_objects_on_table_final.obj")
 
 for j, obj in enumerate(objs):
     obj_list = create_list_from_id2json()
-
     print(obj.get_name())
     if obj.get_name() in obj_list:
         obj_id = get_id_of_object(obj.get_name())
